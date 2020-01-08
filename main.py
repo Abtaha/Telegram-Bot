d1b45127d7f1166f2bbb6decadbc458fe332f356
Abtaha <abtaha2005@gmail.com>
2020-01-08 16:30:27 +0500

    Added Arguments

--- main.py
+++ main.py
@@ -6,10 +6,8 @@ from telegram.ext import CommandHandler
 
 def getLinks(data):
     links = []
-    for a in data:
-        for x in a.items():
-            if x[0] == 'forks_url':
-                links.append(x[1] + "\n")
+    for entry in data:
+        links.append([entry['name'], entry['forks_url']])
     return links
 
 def getData(url):
@@ -20,9 +18,21 @@ data = getData('https://api.github.com/orgs/fedora-infra/repos')
 links = getLinks(data)
 
 
-def sendLinks(update, context):
-    for link in links:
-        context.bot.send_message(chat_id=update.effective_chat.id, text=link)
+def sendForks(update, context):
+    args = "".join(context.args).strip()
+    if args == "":
+        for link in links:
+            context.bot.send_message(chat_id=update.effective_chat.id, text=link[1])
+    else:
+        # These are the links by that particular person
+        sub_links = []
+        for link in links:
+            if link[0] == args:
+                sub_links.append(link[1])
+        
+        sub_links.insert(0, f'{args} has {len(sub_links)} repos. They are: ')
+        sub_links = "\n".join(sub_links)
+        context.bot.send_message(chat_id=update.effective_chat.id, text=sub_links)
 
 updater = Updater(token='1056953929:AAGKVZpg4OR6NgQMeGesXl9u2nfjMRJMocc', use_context=True)
 dispatcher = updater.dispatcher
@@ -31,7 +41,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                      level=logging.INFO)
 
 
-start_handler = CommandHandler('Links', sendLinks)
+start_handler = CommandHandler('forks', sendForks)
 dispatcher.add_handler(start_handler)
 
 updater.start_polling()
